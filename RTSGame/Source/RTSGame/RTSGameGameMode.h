9dd4977983acdb9e00db557065671c74ada935c1
mark visbech hansen <markhansen1990@gmail.com>
2016-05-20 15:15:53 +0200

    c++

---
+++ RTSGame/Source/RTSGame/RTSGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RTSGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTSGAME_API ARTSGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
